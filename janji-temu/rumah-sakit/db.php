adb7764e1dce5c1153b62897b27a21b84e6413e7
ryza <fakhryzaa23@gmail.com>
2026-05-09 10:38:30 +0700

    add php

--- janji-temu/rumah-sakit/db.php
+++ janji-temu/rumah-sakit/db.php
@@ -3,5 +3,13 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "db";
+$database = "db_rumah-sakit";
 
+try {
+    $conn = new PDO ("mysql:host=$host;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
