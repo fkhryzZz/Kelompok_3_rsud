85c5f2c0413726d948f043eebba3ec2ca50f768b
fshoffii <s.shoffii@gmail.com>
2026-05-09 18:52:51 +0700

    Update index.php

--- dokter/index.php
+++ dokter/index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     
-    <link rel="stylesheet" href="../dokter/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="bg-light">
 
